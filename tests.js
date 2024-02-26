c01a8c9adabf889f6e1e2c08c89d58508d48c319
amsahazi <msahazia0@gmail.com>
2024-02-26 01:34:28 +0100

    update javascript

---
+++ tests.js
@@ -0,0 +1,41 @@
+//let OrangeQuantity = 3;
+//let MangoQuantity = 8;
+//let totalFruitsQuantity = OrangeQuantity + MangoQuantity; 
+//let Message = "Bonjour Abdallah votre note est :";
+//let message = "Bonjour Abdallah vous avez :";
+//message = "Bonjour Rouchdi vous possedez :";
+//console.log (message, OrangeQuantity,"Orange et", MangoQuantity, "Mange")
+
+/*const num = 255;
+let txt ="255";
+let bin = true;
+let flt = 7.23;*/
+
+/*let text1 = "je suis";
+let text2 = "un testeur";
+console.log(text1+text2);
+console.log(text1+' '+text2)
+console.log(`${text1}${text2}`)
+console.log(`${text1} ${text2}`)*/
+/*
+// exercice 1 apprendre a creer un tableau et manipuler un tableau
+let daysOfTheWeeks = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+console.log(daysOfTheWeeks[2]);*/
+
+// exercice 2 
+let listOfNumber = [1,2,3,4,5,6,7,8,9]; // creation d'un tableau de donnée de type nombre
+console.log(listOfNumber[1]);// affichage du deuxieme element du tableau
+let item = listOfNumber[1];// stocker l'element [1] ds une vrible item
+listOfNumber[1] = 50;// remplcer la valeur [1] par 50
+console.log(listOfNumber[1])
+
+
+
+
+
+
+
+
+
+
+
