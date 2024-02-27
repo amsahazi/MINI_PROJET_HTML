189f4d1907841b787e70846d8dd60725acbcfbf5
amsahazi <msahazia0@gmail.com>
2024-02-27 01:46:28 +0100

    mise a jour  javascript

--- tests.js
+++ tests.js
@@ -20,7 +20,7 @@ console.log(`${text1} ${text2}`)*/
 /*
 // exercice 1 apprendre a creer un tableau et manipuler un tableau
 let daysOfTheWeeks = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
-console.log(daysOfTheWeeks[2]);*/
+console.log(daysOfTheWeeks[2]);
 
 // exercice 2 
 let listOfNumber = [1,2,3,4,5,6,7,8,9]; // creation d'un tableau de donnée de type nombre
@@ -28,7 +28,98 @@ console.log(listOfNumber[1]);// affichage du deuxieme element du tableau
 let item = listOfNumber[1];// stocker l'element [1] ds une vrible item
 listOfNumber[1] = 50;// remplcer la valeur [1] par 50
 console.log(listOfNumber[1])
+listOfNumber.push(30)// pour ajouter a la fin
+console.log(listOfNumber)
+listOfNumber.unshift(0)
+console.log(listOfNumber)
 
+//exercice 1 pour les objets
+let trainerProfile = {
+    name:"Abdallah",
+    age:32,
+    availble:false
+};
+console.log(trainerProfile["name"])
+console.log(trainerProfile.age)*/
+
+// exercice 2
+let coursAutomation = {
+    "data":[
+       {
+          "uuid":"7dafa230-0eb5-4870-ae79-e5e5a764e046",
+          "name":"Terminal",
+          "slug":"terminal",
+          "order":1,
+          "price":49,
+          "status":"PUBLISHED",
+          "enforce_progression_order":true,
+          "counters":{
+             "nb_customer":11,
+             "nb_abandoned_cart":1,
+             "nb_pre_launch":0,
+             "nb_training_items":13,
+             "nb_draft_training_items":2
+          },
+          "created_at":"2022-05-05 12:51:47",
+          "updated_at":"2022-09-18 19:14:18"
+       },
+       {
+          "uuid":"a66a1b4f-c36d-4cb5-80bb-07f95f3585f6",
+          "name":"Git et GitHub",
+          "slug":"git",
+          "order":2,
+          "price":99,
+          "status":"PUBLISHED",
+          "enforce_progression_order":true,
+          "counters":{
+             "nb_customer":12,
+             "nb_abandoned_cart":0,
+             "nb_pre_launch":0,
+             "nb_training_items":17,
+             "nb_draft_training_items":0
+          },
+          "created_at":"2022-01-22 07:58:37",
+          "updated_at":"2022-09-18 19:14:30"
+       },
+       {
+          "uuid":"1c26c9cd-e184-4621-9b60-9eea426e8aa7",
+          "name":"HTML \/ CSS Basics",
+          "slug":"html-css",
+          "order":3,
+          "price":49,
+          "status":"PUBLISHED",
+          "enforce_progression_order":true,
+          "counters":{
+             "nb_customer":11,
+             "nb_abandoned_cart":0,
+             "nb_pre_launch":0,
+             "nb_training_items":10,
+             "nb_draft_training_items":2
+          },
+          "created_at":"2022-05-30 14:03:17",
+          "updated_at":"2022-09-18 19:14:46"
+       }   
+    ],
+    "links":{
+       "first":"https:\/\/api.teachizy.fr\/api\/v1\/trainings?page=1",
+       "last":"https:\/\/api.teachizy.fr\/api\/v1\/trainings?page=1",
+       "prev":null,
+       "next":null
+    },
+    "meta":{
+       "current_page":1,
+       "from":1,
+       "last_page":1,
+       "path":"https:\/\/api.teachizy.fr\/api\/v1\/trainings",
+       "per_page":100,
+       "to":8,
+       "total":8
+    }
+ }
+ 
+ console.log(coursAutomation.data[2].updated_at)// 1er methode pour afficher (updtae_at htmlcss)
+let htmlcss = coursAutomation.data[2];
+console.log(htmlcss.updated_at)// 2em methode 
 
 
 
